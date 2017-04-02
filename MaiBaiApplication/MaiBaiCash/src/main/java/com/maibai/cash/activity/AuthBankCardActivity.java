896b3f2776bc6b5dc78cd4394199be63068d1707
cuiyue <380920705@qq.com>
2017-04-02 14:47:39 +0800

    认证银行卡UI

--- MaiBaiApplication/MaiBaiCash/src/main/java/com/maibai/cash/activity/AuthBankCardActivity.java
+++ MaiBaiApplication/MaiBaiCash/src/main/java/com/maibai/cash/activity/AuthBankCardActivity.java
@@ -2,16 +2,47 @@ package com.maibai.cash.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.maibai.cash.R;
 import com.maibai.cash.base.BaseActivity;
 import com.maibai.cash.utils.ToastUtil;
+
+import butterknife.BindView;
 
 /**
  * 银行卡信息
  */
 
 public class AuthBankCardActivity extends BaseActivity implements View.OnClickListener {
+
+    @BindView(R.id.tv_auth_bank_card_back)
+    TextView tvAuthBankCardBack;
+    @BindView(R.id.tv_auth_info_post)
+    TextView tvAuthInfoPost;
+    @BindView(R.id.tv_auth_bank_card_person)
+    TextView tvAuthBankCardPerson;
+    @BindView(R.id.et_auth_bank_card_person)
+    EditText etAuthBankCardPerson;
+    @BindView(R.id.tv_bank_card_key)
+    TextView tvBankCardKey;
+    @BindView(R.id.et_bank_card)
+    EditText etBankCard;
+    @BindView(R.id.tv_bank_card_num_key)
+    TextView tvBankCardNumKey;
+    @BindView(R.id.et_auth_info_work_name)
+    EditText etAuthInfoWorkName;
+    @BindView(R.id.tv_bank_card_phone_num_key)
+    TextView tvBankCardPhoneNumKey;
+    @BindView(R.id.et_bank_card_phone_num)
+    EditText etBankCardPhoneNum;
+    @BindView(R.id.tv_severity_code_key)
+    TextView tvSeverityCodeKey;
+    @BindView(R.id.et_severity_code)
+    EditText etSeverityCode;
+    @BindView(R.id.tv_severity_code)
+    TextView tvSeverityCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
