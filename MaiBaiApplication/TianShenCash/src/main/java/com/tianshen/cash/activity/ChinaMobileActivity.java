f92dfe0416542da18181f1b43261cdea03a24a0b
cuiyue <380920705@qq.com>
2017-04-07 16:17:16 +0800

    增加JS调用安卓接口

--- MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/ChinaMobileActivity.java
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/ChinaMobileActivity.java
@@ -1,14 +1,18 @@
 package com.tianshen.cash.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tianshen.cash.R;
 import com.tianshen.cash.base.BaseActivity;
+import com.tianshen.cash.utils.ToastUtil;
 
 import butterknife.BindView;
 
@@ -58,6 +62,7 @@ public class ChinaMobileActivity extends BaseActivity implements View.OnClickLis
         webSettings.setJavaScriptEnabled(true); //支持js
         webSettings.setSupportZoom(true); //支持缩放
         webSettings.setAppCacheEnabled(true);
+        webSettings.setDefaultTextEncodingName("UTF-8");
         webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
     }
 
@@ -65,6 +70,7 @@ public class ChinaMobileActivity extends BaseActivity implements View.OnClickLis
 
         String url = "http://118.190.83.21:8081/h5/mobile_auth/mobile_auth.html";
         wvChinaMobile.loadUrl(url);
+        wvChinaMobile.addJavascriptInterface(new JSCallback(), "android");
         wvChinaMobile.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -74,6 +80,16 @@ public class ChinaMobileActivity extends BaseActivity implements View.OnClickLis
                 return true;
             }
         });
+    }
+
+    public class JSCallback {
+
+        //将显示Toast和对话框的方法暴露给JS脚本调用
+        @JavascriptInterface
+        public void showToast(String str) {
+            ToastUtil.showToast(mContext, "JS-->" + str);
+        }
+
     }
 
     @Override
