a45222889423e838d077406bbbad033209e0bbb5
cuiyue <380920705@qq.com>
2017-04-10 15:52:54 +0800

    确认银行页面增加上传通信录和通话记录

--- MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/ConfirmBankCardActivity.java
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/ConfirmBankCardActivity.java
@@ -1,11 +1,16 @@
 package com.tianshen.cash.activity;
 
+import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.tianshen.cash.R;
 import com.tianshen.cash.base.BaseActivity;
+import com.tianshen.cash.constant.GlobalParams;
 import com.tianshen.cash.event.ApplyEvent;
+import com.tianshen.cash.fragment.WithdrawalsFragment;
+import com.tianshen.cash.utils.MainUtil;
+import com.tianshen.cash.utils.UploadToServerUtil;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -18,6 +23,13 @@ import butterknife.BindView;
 public class ConfirmBankCardActivity extends BaseActivity implements View.OnClickListener {
 
 
+    private UploadToServerUtil uploadUtil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uploadCallContacts();
+    }
 
     @Override
     protected int setContentView() {
@@ -46,6 +58,51 @@ public class ConfirmBankCardActivity extends BaseActivity implements View.OnClic
      * 点击了确认
      */
     private void onClickApply() {
+    }
+
+
+    /**
+     * 上传通信录和联系人
+     */
+    private void uploadCallContacts() {
+        uploadUtil = new UploadToServerUtil(mContext);
+        uploadUtil.setCallBack(new MyUploadCallBack());
+        uploadUtil.uploadUserInfo(GlobalParams.UPLOADCALLRECORD);
+    }
+
+    private class MyUploadCallBack implements UploadToServerUtil.UploadCallBack {
+
+        @Override
+        public void uploadSuccessCallBack(int type) {
+            //上传通讯录、通话记录、短信等的回调
+            switch (type) {
+                case GlobalParams.UPLOADCALLCONTACTS:
+                    //上传联系人成功
+                    break;
+                case GlobalParams.UPLOADCALLRECORD:
+                    //上传通话记录成功
+                    uploadUtil.uploadUserInfo(GlobalParams.UPLOADCALLCONTACTS);
+                    break;
+                case GlobalParams.UPLOADMESSAGE:
+                    //上传短信成功
+                    break;
+            }
+        }
+
+        @Override
+        public void uploadFailCallBack(int type) {
+            switch (type) {
+                case GlobalParams.UPLOADCALLCONTACTS:
+                    //上传联系人失败
+                    break;
+                case GlobalParams.UPLOADCALLRECORD:
+                    //上传通话记录失败
+                    break;
+                case GlobalParams.UPLOADMESSAGE:
+                    //上传短信失败
+                    break;
+            }
+        }
     }
 
 }
