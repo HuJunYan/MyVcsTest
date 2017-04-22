d39b033c6ce88c9a159397c1405cf18bfa6b9676
cuiyue <380920705@qq.com>
2017-04-22 11:08:55 +0800

    掌众还款

---
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/net/api/payConfirmZhangzhong.java
@@ -0,0 +1,54 @@
+package com.tianshen.cash.net.api;
+
+import android.content.Context;
+import android.view.View;
+
+import com.tianshen.cash.constant.NetConstantValue;
+import com.tianshen.cash.model.PostDataBean;
+import com.tianshen.cash.net.base.BaseNetCallBack;
+import com.tianshen.cash.net.base.CallBack;
+import com.tianshen.cash.net.base.GsonUtil;
+import com.tianshen.cash.net.base.NetBase;
+import com.tianshen.cash.utils.SignUtils;
+
+import org.json.JSONObject;
+
+public class PayConfirmZhangzhong extends NetBase {
+
+    private String mUrl;
+    private JSONObject mJSONObject;
+
+    public PayConfirmZhangzhong(Context context) {
+        super(context);
+        mUrl = NetConstantValue.getPayConfirmZhangzhongUrl();
+    }
+
+    public void payConfirm(JSONObject jsonObject, View view, boolean isShowDialog, final BaseNetCallBack<PostDataBean> callBack) {
+        mJSONObject = SignUtils.signJsonNotContainList(jsonObject);
+        if (mJSONObject == null) {
+            return;
+        }
+        getDataFromServerByPost(mUrl, mJSONObject, view, isShowDialog, new CallBack() {
+            @Override
+            public void onSuccess(String result, String url) {
+                successHandle(result, url, callBack);
+            }
+
+            @Override
+            public void onFailure(String result, int errorType, int errorCode) {
+                failureHandle(result, errorType, errorCode, callBack);
+            }
+        });
+    }
+
+    private void successHandle(String result, String url, BaseNetCallBack<PostDataBean> callBack) {
+        PostDataBean postDataBean = GsonUtil.json2bean(result, PostDataBean.class);
+        callBack.onSuccess(postDataBean);
+    }
+
+    private void failureHandle(String result, int errorType, int errorCode, BaseNetCallBack<PostDataBean> callBack) {
+        callBack.onFailure(result, errorType, errorCode);
+    }
+
+
+}
