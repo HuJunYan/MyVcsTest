5106e46e2b97305f387ec1386652d0723bb034cc
cuiyue <380920705@qq.com>
2017-05-04 01:57:07 +0800

    修改版本号 2.0

--- MaiBaiApplication/build.sh
+++ MaiBaiApplication/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 currentApplicationName='TianShenCash'
 
-currentApkTrailName="'_$currentApplicationName\_1-0-4_04201033.apk'"
-currentVersionCode="104"
-currentVersionName='"1.0.4"'
+currentApkTrailName="'_$currentApplicationName\_2-0-0_05040140.apk'"
+currentVersionCode="200"
+currentVersionName='"2.0.0"'
 
 
 funChangeVersionCode() {
@@ -77,21 +77,21 @@ funBuild(){
 
 #funBuild   ceshi                1
 funBuild   server               2000
-#funBuild   wandoujia            2001
-#funBuild   qqTencent            2002
-#funBuild   zs91zhushou          2003
-#funBuild   huawei               2004
-#funBuild   xiaomi               2005
-#funBuild   baidu                2006
-#funBuild   anzhi                2007
-#funBuild   oppo                 2008
-#funBuild   appchina             2009
-#funBuild   lenovo               2010
-#funBuild   meizu                2011
-#funBuild   baidusem             2012
-#funBuild   UCWeb                2013
-#funBuild   dev360cn             2014
-#funBuild   vivo                 2015
+funBuild   wandoujia            2001
+funBuild   qqTencent            2002
+funBuild   zs91zhushou          2003
+funBuild   huawei               2004
+funBuild   xiaomi               2005
+funBuild   baidu                2006
+funBuild   anzhi                2007
+funBuild   oppo                 2008
+funBuild   appchina             2009
+funBuild   lenovo               2010
+funBuild   meizu                2011
+funBuild   baidusem             2012
+funBuild   UCWeb                2013
+funBuild   dev360cn             2014
+funBuild   vivo                 2015
 
 
 
