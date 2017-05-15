94874fa69b689981a6c5b32770f55aaeb1cb36e0
cuiyue <380920705@qq.com>
2017-05-15 11:14:37 +0800

    修改地址

--- MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/constant/NetConstantValue.java
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/constant/NetConstantValue.java
@@ -13,10 +13,10 @@ public class NetConstantValue {
 //    public static String HOST = "http://tsdapi.tinybun.top/";
 
     //测试
-//    public static String HOST = "http://115.182.49.78/";
+    public static String HOST = "http://115.182.49.78/";
 
     //测试
-    public static String HOST = "http://118.190.83.21/";
+//    public static String HOST = "http://118.190.83.21/";
 
 
     public static String COMMONURI = HOST + "Home/";
