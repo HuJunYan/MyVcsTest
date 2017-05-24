d77cd48e1d7a3bc85cbc3f6c900718ec45799e71
cuiyue <380920705@qq.com>
2017-05-24 17:02:33 +0800

    增加借款超市页面

---
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/SuperMarkerActivity.java
@@ -0,0 +1,27 @@
+package com.tianshen.cash.activity;
+
+import com.tianshen.cash.R;
+import com.tianshen.cash.base.BaseActivity;
+
+/**
+ * Created by cuiyue on 2017/5/24.
+ */
+
+public class SuperMarkerActivity extends BaseActivity {
+
+
+    @Override
+    protected int setContentView() {
+        return R.layout.activity_super_marker;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void setListensers() {
+
+    }
+}
