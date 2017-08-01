5c7ba077bc76e1e1c926b0033955ca86f6dfecb8
cuiyue <380920705@qq.com>
2017-08-01 17:40:42 +0800

    创建新的借款记录页面

---
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/LoanHistoryActivity.kt
@@ -0,0 +1,17 @@
+package com.tianshen.cash.activity
+
+import com.tianshen.cash.R
+import com.tianshen.cash.base.BaseActivity
+
+class LoanHistoryActivity : BaseActivity() {
+
+    override fun setContentView(): Int = R.layout.activity_loan_history
+
+    override fun findViews() {
+    }
+
+    override fun setListensers() {
+    }
+
+
+}
