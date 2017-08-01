b19bb9a8187c3e1d6bba9f2f3211dbfe22cda7be
Dr.Wang <80250706@qq.com>
2017-08-01 14:12:11 +0800

    更改状态栏颜色为白色3

--- MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/utils/RomUtils.java
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/utils/RomUtils.java
@@ -35,31 +35,15 @@ public class RomUtils {
     }
 
     /**
-     * 设置状态栏图标为深色和魅族特定的文字风格，Flyme4.0以上
-     * 可以用来判断是否为Flyme用户
+     * 用来判断是否为Flyme用户
      *
-     * @param window 需要设置的窗口
-     * @param dark   是否把状态栏字体及图标颜色设置为深色
      * @return boolean 成功执行返回true
-     * 本方法参数为window  已经boolean 值 dark   因为不需要设置 暂时删除掉了 .用到的时候加上
      */
-    public static boolean FlymeSetStatusBarLightMode(/*Window window,boolean dark*/) {
+    public static boolean FlymeSetStatusBarLightMode() {
         boolean result = false;
         try {
-//                WindowManager.LayoutParams lp = window.getAttributes();
             Field darkFlag = WindowManager.LayoutParams.class.getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
             Field meizuFlags = WindowManager.LayoutParams.class.getDeclaredField("meizuFlags");
-//                darkFlag.setAccessible(true);
-//                meizuFlags.setAccessible(true);
-//                int bit = darkFlag.getInt(null);
-//                int value = meizuFlags.getInt(lp);
-//                if (dark) {
-//                    value |= bit;
-//                } else {
-//                    value &= ~bit;
-//                }
-//                meizuFlags.setInt(lp, value);
-//                window.setAttributes(lp);
             result = true;
         } catch (Exception ignored) {
         }
