7e3b30d35b15db27613a4c9fc7e083b102b0f5dd
Dr.Wang <80250706@qq.com>
2017-08-25 11:49:14 +0800

    修改逻辑

--- MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/view/RepayDetailDialogView.java
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/view/RepayDetailDialogView.java
@@ -44,8 +44,6 @@ public class RepayDetailDialogView {
                 layoutParams.width = DensityUtil.dp2px(300);
                 contentView.setLayoutParams(layoutParams);
                 dialog.getWindow().setGravity(Gravity.CENTER);
-                loadData(contentView, paramT);
-                processShowOrHide(contentView, paramT);
                 if (!activity.isFinishing()) {
                     dialog.show();
                 }
@@ -59,80 +57,4 @@ public class RepayDetailDialogView {
 
     }
 
-    /**
-     * 加载展示的数据
-     *
-     * @param contentView
-     * @param paramT
-     */
-    private void loadData(View contentView, UserRepayDetailBean paramT) {
-        UserRepayDetailBean.Detail data = paramT.data;
-        //利率
-        TextView tv_annual_interest_rate = (TextView) contentView.findViewById(R.id.tv_annual_interest_rate);
-        //应还本金
-        TextView tv_consume_amount = (TextView) contentView.findViewById(R.id.tv_consume_amount);
-        //支付利息
-        TextView tv_interest = (TextView) contentView.findViewById(R.id.tv_interest);
-        //手续费用
-        TextView tv_service_charge = (TextView) contentView.findViewById(R.id.tv_service_charge);
-        //滞纳金
-        TextView tv_late_fee = (TextView) contentView.findViewById(R.id.tv_late_fee);
-        //逾期罚息
-        TextView tv_late_charge = (TextView) contentView.findViewById(R.id.tv_late_charge);
-        //逾期管理费
-        TextView tv_overdue_management_fee = (TextView) contentView.findViewById(R.id.tv_overdue_management_fee);
-        //应还款总金额
-        TextView tv_collect_customer_money = (TextView) contentView.findViewById(R.id.tv_collect_customer_money);
-        setTextView(tv_consume_amount, data.consume_amount);
-        setTextView(tv_interest, data.interest);
-        setTextView(tv_service_charge, data.service_charge);
-        setTextView(tv_late_fee, data.late_fee);
-        setTextView(tv_late_charge, data.late_charge);
-        setTextView(tv_overdue_management_fee, data.overdue_management_fee);
-        setTextView(tv_collect_customer_money, data.collect_customer_money);
-        tv_annual_interest_rate.setText("年利率" + Float.parseFloat(data.annual_interest_rate) * 100f + "%");
-    }
-
-    private void setTextView(TextView textview, String money) {
-        try {
-            textview.setText(MoneyUtils.changeF2Y(money) + "元");
-        } catch (Exception e) {
-        }
-    }
-
-    private void processShowOrHide(View contentView, UserRepayDetailBean paramT) {
-        UserRepayDetailBean.Detail data = paramT.data;
-        //本金item
-        View ll_consume_amount = contentView.findViewById(R.id.ll_consume_amount);
-        String consume_amount = data.consume_amount;
-        processItem(ll_consume_amount, consume_amount);
-        //支付利息
-        View ll_interest = contentView.findViewById(R.id.ll_interest);
-        String interest = data.interest;
-        processItem(ll_interest, interest);
-        //手续费用
-        View ll_service_charge = contentView.findViewById(R.id.ll_service_charge);
-        String service_charge = data.service_charge;
-        processItem(ll_service_charge, service_charge);
-        //滞纳金
-        View ll_late_fee = contentView.findViewById(R.id.ll_late_fee);
-        String late_fee = data.late_fee;
-        processItem(ll_late_fee, late_fee);
-        //逾期罚息
-        View ll_late_charge = contentView.findViewById(R.id.ll_late_charge);
-        String late_charge = data.late_charge;
-        processItem(ll_late_charge, late_charge);
-        //逾期管理费
-        View ll_overdue_management_fee = contentView.findViewById(R.id.ll_overdue_management_fee);
-        String overdue_management_fee = data.overdue_management_fee;
-        processItem(ll_overdue_management_fee, overdue_management_fee);
-
-    }
-
-    private void processItem(View view, String money) {
-        long l = Long.parseLong(money);
-        if (l == 0) {
-            view.setVisibility(View.GONE);
-        }
-    }
 }
