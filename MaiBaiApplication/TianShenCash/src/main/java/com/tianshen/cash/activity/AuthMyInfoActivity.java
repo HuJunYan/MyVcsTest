d58ca98843da1f3c0767ca64372a190abe9f9a2c
hujunyan <1435074357@qq.com>
2017-11-06 15:21:24 +0800

    个人认证中心界面入口activity

---
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/AuthMyInfoActivity.java
@@ -0,0 +1,33 @@
+package com.tianshen.cash.activity;
+
+import android.os.Bundle;
+
+import com.tianshen.cash.R;
+import com.tianshen.cash.base.BaseActivity;
+
+/**
+ * 个人信息认证入口view
+ *
+ */
+public class AuthMyInfoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int setContentView() {
+        return R.layout.activity_auth_my_info;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void setListensers() {
+
+    }
+}
