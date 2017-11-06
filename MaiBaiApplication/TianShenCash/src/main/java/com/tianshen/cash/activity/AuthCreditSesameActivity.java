b8ea64a0dc78f10bd36e84d219a3f22f118eb433
hujunyan <1435074357@qq.com>
2017-11-06 17:12:23 +0800

    个人信息认证简单界面+芝麻信用简单界面

---
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/AuthCreditSesameActivity.java
@@ -0,0 +1,30 @@
+package com.tianshen.cash.activity;
+
+import android.os.Bundle;
+
+import com.tianshen.cash.R;
+import com.tianshen.cash.base.BaseActivity;
+
+public class AuthCreditSesameActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_credit_sesame);
+    }
+
+    @Override
+    protected int setContentView() {
+        return 0;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void setListensers() {
+
+    }
+}
---
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/AuthPersonInfoActivity.java
@@ -0,0 +1,33 @@
+package com.tianshen.cash.activity;
+
+import android.os.Bundle;
+
+import com.tianshen.cash.R;
+import com.tianshen.cash.base.BaseActivity;
+
+/**
+ * 个人信息认证
+ */
+public class AuthPersonInfoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_person_info);
+    }
+
+    @Override
+    protected int setContentView() {
+        return 0;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void setListensers() {
+
+    }
+}
