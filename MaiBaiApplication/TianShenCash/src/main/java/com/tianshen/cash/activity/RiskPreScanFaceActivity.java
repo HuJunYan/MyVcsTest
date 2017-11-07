e9df5d8792f5a364420d1da4caad22c4ffa15eee
Dr.Wang <wangchen3126>
2017-11-07 10:31:36 +0800

    创建新页面

---
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/RiskPreScanFaceActivity.java
@@ -0,0 +1,23 @@
+package com.tianshen.cash.activity;
+
+import com.tianshen.cash.R;
+import com.tianshen.cash.base.BaseActivity;
+
+public class RiskPreScanFaceActivity extends BaseActivity {
+
+
+    @Override
+    protected int setContentView() {
+        return R.layout.activity_risk_pre_scan_face;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void setListensers() {
+
+    }
+}
