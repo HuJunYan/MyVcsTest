6e897ce38e920638e71046f6572d711868f3e1c8
cuiyue <380920705@qq.com>
2017-11-07 18:45:49 +0800

    认证中心页面跳转逻辑修改

--- MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/AuthCenterMenuActivity.kt
+++ MaiBaiApplication/TianShenCash/src/main/java/com/tianshen/cash/activity/AuthCenterMenuActivity.kt
@@ -2,6 +2,7 @@ package com.tianshen.cash.activity
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
 import com.tianshen.cash.R
 import com.tianshen.cash.adapter.MyViewPagerAdapter
 import com.tianshen.cash.base.BaseActivity
@@ -16,33 +17,60 @@ import kotlinx.android.synthetic.main.activity_auth_center_menu.*
 import org.json.JSONObject
 import java.util.*
 
-/**
- * Created by cuiyue on 2017/11/6.
- */
 class AuthCenterMenuActivity : BaseActivity() {
 
     private var mFragmentList: ArrayList<Fragment> = ArrayList()
     private var mViewPagerAdapter: MyViewPagerAdapter? = null
+    private var mCurrentIndex: Int = 0
 
     override fun setContentView() = R.layout.activity_auth_center_menu
 
     override fun findViews() {
+        tv_goto_auth.setOnClickListener {
+            when (mCurrentIndex) {
+                0 -> {
+                    gotoActivity(mContext, RiskPreAuthIdentityActivity::class.java, null)
+                }
+                1 -> {
+                    val bundle = Bundle()
+                    bundle.putString(AuthMyInfoActivity.ACTIVITY_FLAG, AuthMyInfoActivity.PERSONFLAG)
+                    gotoActivity(mContext, AuthMyInfoActivity::class.java, bundle)
+                }
+                2 -> {
+                    val bundle = Bundle()
+                    bundle.putString(AuthMyInfoActivity.ACTIVITY_FLAG, AuthMyInfoActivity.CREDITFLAG)
+                    gotoActivity(mContext, AuthMyInfoActivity::class.java, bundle)
+                }
+            }
+        }
+
+        vp_auth_center_menu.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                mCurrentIndex = position
+            }
+
+        })
     }
 
     override fun setListensers() {
         tv_auth_center_menu_back.setOnClickListener { backActivity() }
-        tv_identity.setOnClickListener {
-            gotoActivity(mContext, RiskPreAuthIdentityActivity::class.java, null)
+        rl_identity.setOnClickListener {
+            vp_auth_center_menu.currentItem = 0
+            mCurrentIndex = 0
         }
-        tv_person_info.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putString(AuthMyInfoActivity.ACTIVITY_FLAG, AuthMyInfoActivity.PERSONFLAG)
-            gotoActivity(mContext, AuthMyInfoActivity::class.java, bundle)
+        rl_person_info.setOnClickListener {
+            vp_auth_center_menu.currentItem = 1
+            mCurrentIndex = 1
         }
-        tv_credit.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putString(AuthMyInfoActivity.ACTIVITY_FLAG, AuthMyInfoActivity.CREDITFLAG)
-            gotoActivity(mContext, AuthMyInfoActivity::class.java, bundle)
+        rl_credit.setOnClickListener {
+            vp_auth_center_menu.currentItem = 2
+            mCurrentIndex = 2
         }
     }
 
@@ -67,8 +95,8 @@ class AuthCenterMenuActivity : BaseActivity() {
 
     private fun initViewPager() {
         mViewPagerAdapter = MyViewPagerAdapter(supportFragmentManager, mFragmentList)
-        vp_auth_center_menu.setAdapter(mViewPagerAdapter)
-        vp_auth_center_menu.setOffscreenPageLimit(mFragmentList.size)
+        vp_auth_center_menu.adapter = mViewPagerAdapter
+        vp_auth_center_menu.offscreenPageLimit = mFragmentList.size
     }
 
     private fun initData() {
@@ -98,9 +126,9 @@ class AuthCenterMenuActivity : BaseActivity() {
         var fragmentStep2 = mFragmentList[1] as AuthCenterMenuFragment
         var fragmentStep3 = mFragmentList[2] as AuthCenterMenuFragment
 
-        fragmentStep1.setPicAndTxt(R.drawable.ic_auth_center_menu_pic_1, "测试文字2222")
-        fragmentStep2.setPicAndTxt(R.drawable.ic_auth_center_menu_pic_2, "测试文字3333")
-        fragmentStep3.setPicAndTxt(R.drawable.ic_auth_center_menu_pic_3, "测试文字4444")
+        fragmentStep1.setPicAndTxt(R.drawable.ic_auth_center_menu_pic_1, "公安部联网认证")
+        fragmentStep2.setPicAndTxt(R.drawable.ic_auth_center_menu_pic_2, "填写真实信息，顺利通过认证")
+        fragmentStep3.setPicAndTxt(R.drawable.ic_auth_center_menu_pic_3, "信用贷款，还有机会提升额度")
     }
 
 }
